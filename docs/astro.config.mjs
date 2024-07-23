2ebc7c65e63572b2f50239b95ecc339efed541d0
Anthony <brnaba@amazon.fr>
2024-07-23 15:37:48 +0200

    removed base path

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -4,7 +4,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: process.env.ASTRO_SITE,
-	base: '/multi-agent-orchestrator',
+	base: '/',
 	markdown: {
 		gfm: true
   },
