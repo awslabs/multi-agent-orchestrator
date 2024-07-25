324c592149827bccb6fc1291062777904b9e40bb
Anthony <brnaba@amazon.fr>
2024-07-25 16:47:57 +0200

    added linting

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'prettier'
+    ],
+    rules: {
+      // Add custom rules here
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/ban-ts-comment": "off"
+    },
+  };
