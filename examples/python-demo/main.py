12dcfa51f3f1c6a0153d2455840a4b688aea6e63
Anthony <brnaba@amazon.fr>
2024-08-02 17:34:59 +0200

    linting fix

--- examples/python-demo/main.py
+++ examples/python-demo/main.py
@@ -3,6 +3,7 @@ import uuid
 import asyncio
 from typing import Optional, List, Dict, Any
 import json
+import sys
 
 from multi_agent_orchestrator.orchestrator import MultiAgentOrchestrator, OrchestratorConfig
 from multi_agent_orchestrator.agents import (BedrockLLMAgent,
@@ -15,11 +16,10 @@ class BedrockLLMAgentCallbacks(AgentCallbacks):
     def on_llm_new_token(self, token: str) -> None:
         # handle response streaming here
         print(token, end='', flush=True)
-        pass
 
 
-async def handle_request(orchestrator, user_input, user_id, session_id):
-    response:AgentResponse = await orchestrator.route_request(user_input, user_id, session_id)
+async def handle_request(_orchestrator: MultiAgentOrchestrator, _user_input:str, _user_id:str, _session_id:str):
+    response:AgentResponse = await _orchestrator.route_request(_user_input, _user_id, _session_id)
 
     # Print metadata
     print("\nMetadata:")
@@ -29,56 +29,63 @@ async def handle_request(orchestrator, user_input, user_id, session_id):
     else:
         print('Response:', response.output.content[0]['text'])
 
-def custom_input_payload_encoder(input_text: str, chat_history: List[Any], user_id: str, session_id: str, additional_params: Optional[Dict[str, str]] = None) -> str:
+def custom_input_payload_encoder(input_text: str,
+                                 chat_history: List[Any],
+                                 user_id: str,
+                                 session_id: str,
+                                 additional_params: Optional[Dict[str, str]] = None) -> str:
     return json.dumps({
         'hello':'world'
     })
 
 def custom_output_payload_decoder(response: Dict[str, Any]) -> Any:
-    decoded_response = json.loads(json.loads(response['Payload'].read().decode('utf-8'))['body'])['response']
+    decoded_response = json.loads(
+        json.loads(
+            response['Payload'].read().decode('utf-8')
+        )['body'])['response']
     return ConversationMessage(
             role=ParticipantRole.ASSISTANT.value,
             content=[{'text': decoded_response}]
         )
 
+if __name__ == "__main__":
 
-# Initialize the orchestrator with some options
-orchestrator = MultiAgentOrchestrator(options=OrchestratorConfig(
-    LOG_AGENT_CHAT=True,
-    LOG_CLASSIFIER_CHAT=True,
-    LOG_CLASSIFIER_RAW_OUTPUT=True,
-    LOG_CLASSIFIER_OUTPUT=True,
-    LOG_EXECUTION_TIMES=True,
-    MAX_RETRIES=3,
-    USE_DEFAULT_AGENT_IF_NONE_IDENTIFIED=True,
-    MAX_MESSAGE_PAIRS_PER_AGENT=10
-))
+    # Initialize the orchestrator with some options
+    orchestrator = MultiAgentOrchestrator(options=OrchestratorConfig(
+        LOG_AGENT_CHAT=True,
+        LOG_CLASSIFIER_CHAT=True,
+        LOG_CLASSIFIER_RAW_OUTPUT=True,
+        LOG_CLASSIFIER_OUTPUT=True,
+        LOG_EXECUTION_TIMES=True,
+        MAX_RETRIES=3,
+        USE_DEFAULT_AGENT_IF_NONE_IDENTIFIED=True,
+        MAX_MESSAGE_PAIRS_PER_AGENT=10
+    ))
 
-# Add some agents
-tech_agent = BedrockLLMAgent(BedrockLLMAgentOptions(
-    name="Tech Agent",
-    streaming=True,
-    description="Specializes in technology areas including software development, hardware, AI, cybersecurity, blockchain, cloud computing, emerging tech innovations, and pricing/costs related to technology products and services.",
-    model_id="anthropic.claude-3-sonnet-20240229-v1:0",
-    callbacks=BedrockLLMAgentCallbacks()
-))
-orchestrator.add_agent(tech_agent)
+    # Add some agents
+    tech_agent = BedrockLLMAgent(BedrockLLMAgentOptions(
+        name="Tech Agent",
+        streaming=True,
+        description="Specializes in technology areas including software development, hardware, AI, \
+            cybersecurity, blockchain, cloud computing, emerging tech innovations, and pricing/costs \
+            related to technology products and services.",
+        model_id="anthropic.claude-3-sonnet-20240229-v1:0",
+        callbacks=BedrockLLMAgentCallbacks()
+    ))
+    orchestrator.add_agent(tech_agent)
 
+    USER_ID = "user123"
+    SESSION_ID = str(uuid.uuid4())
 
-user_id = "user123"
-session_id = str(uuid.uuid4())
-print("Welcome to the interactive Multi-Agent system. Type 'quit' to exit.")
+    print("Welcome to the interactive Multi-Agent system. Type 'quit' to exit.")
 
+    while True:
+        # Get user input
+        user_input = input("\nYou: ").strip()
 
-while True:
-    # Get user input
-    user_input = input("\nYou: ").strip()
+        if user_input.lower() == 'quit':
+            print("Exiting the program. Goodbye!")
+            sys.exit()
 
-    if user_input.lower() == 'quit':
-        print("Exiting the program. Goodbye!")
-        exit(0)
-
-    # Run the async function
-    asyncio.run(handle_request(orchestrator, user_input, user_id, session_id))
-
-
+        # Run the async function
+        asyncio.run(handle_request(orchestrator, user_input, USER_ID, SESSION_ID))
