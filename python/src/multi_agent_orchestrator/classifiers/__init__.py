0c28f3d9ea45985a2ce90beb3d9f712f674c8721
Anthony Bernabeu <64135631+brnaba-aws@users.noreply.github.com>
2024-11-26 15:56:32 +0100

    re-organized python package dependency and mao installation

--- python/src/multi_agent_orchestrator/classifiers/__init__.py
+++ python/src/multi_agent_orchestrator/classifiers/__init__.py
@@ -1,20 +1,38 @@
 """
 Code for Classifier.
 """
-
-
 from .classifier import Classifier, ClassifierResult
-from .anthropic_classifier import AnthropicClassifier, AnthropicClassifierOptions
 from .bedrock_classifier import BedrockClassifier, BedrockClassifierOptions
-from .openai_classifier import OpenAIClassifier, OpenAIClassifierOptions
+
+try:
+    from .anthropic_classifier import AnthropicClassifier, AnthropicClassifierOptions
+    _ANTHROPIC_AVAILABLE = True
+except Exception as e:
+    _ANTHROPIC_AVAILABLE = False
+
+try:
+    from .openai_classifier import OpenAIClassifier, OpenAIClassifierOptions
+    _OPENAI_AVAILABLE = True
+except Exception as e:
+    _OPENAI_AVAILABLE = False
+
+
 
 __all__ = [
-    "AnthropicClassifier",
-    "AnthropicClassifierOptions",
-    "BedrockClassifier",
-    "BedrockClassifierOptions",
     "Classifier",
     "ClassifierResult",
-    "OpenAIClassifier",
-    "OpenAIClassifierOptions",
+    "BedrockClassifier",
+    "BedrockClassifierOptions"
 ]
+
+if _ANTHROPIC_AVAILABLE:
+    __all__.extend([
+        "AnthropicClassifier",
+        "AnthropicClassifierOptions"
+    ])
+
+if _OPENAI_AVAILABLE:
+    __all__.extend([
+        "OpenAIClassifier",
+        "OpenAIClassifierOptions"
+    ])
