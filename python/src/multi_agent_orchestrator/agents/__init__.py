5c61f04ad5e9cb852575fd030df6fec919234fbc
Anthony Bernabeu <64135631+brnaba-aws@users.noreply.github.com>
2024-11-27 14:28:41 +0100

    moved inline_agent_bedrock

--- python/src/multi_agent_orchestrator/agents/__init__.py
+++ python/src/multi_agent_orchestrator/agents/__init__.py
@@ -9,6 +9,7 @@ from .amazon_bedrock_agent import AmazonBedrockAgent, AmazonBedrockAgentOptions
 from .comprehend_filter_agent import ComprehendFilterAgent, ComprehendFilterAgentOptions
 from .chain_agent import ChainAgent, ChainAgentOptions
 from .bedrock_translator_agent import BedrockTranslatorAgent, BedrockTranslatorAgentOptions
+from .inline_agent_bedrock import BedrockInlineAgent, BedrockInlineAgentOptions
 
 try:
     from .anthropic_agent import AnthropicAgent, AnthropicAgentOptions
@@ -37,6 +38,8 @@ __all__ = [
     'BedrockTranslatorAgentOptions',
     'ChainAgent',
     'ChainAgentOptions',
+    'BedrockInlineAgent',
+    'BedrockInlineAgentOptions'
 ]
 
 if _ANTHROPIC_AVAILABLE:
