702276a7fd4c4bee43af87c1c83e0bf4749de9d4
Corneliu CROITORU <cornel.croitoru@gmail.com>
2024-12-15 12:31:41 +0100

    add badge to readme

---
+++ examples/ecommerce-support-simulator/diagram/venv/lib/python3.11/site-packages/diagrams/oci/__init__.py
@@ -0,0 +1,12 @@
+"""
+OCI provides a set of services for Oracle Cloud Infrastructure provider.
+"""
+
+from diagrams import Node
+
+
+class _OCI(Node):
+    _provider = "oci"
+    _icon_dir = "resources/oci"
+
+    fontcolor = "#312D2A"
