51f90a9eba70cc17f42b3e8e515ebb19c847eef9
Corneliu Croitoru <cornel.croitoru@gmail.com>
2024-12-24 10:46:52 +0100

    minor change to email simulator

--- examples/ecommerce-support-simulator/lambda/customerMessage/sqsLogger.ts
+++ examples/ecommerce-support-simulator/lambda/customerMessage/sqsLogger.ts
@@ -34,7 +34,7 @@ export class SQSLogger {
     }
   }
 
-  
+
 
   private formatMessage(...args: any[]): string {
     return args.map(arg => 
@@ -44,7 +44,6 @@ export class SQSLogger {
 
   log(...args: any[]): void {
     const message = this.formatMessage(...args);
-    console.log("sendToSQS message="+JSON.stringify(message))
     this.sendToSQS(message);
   }
 
