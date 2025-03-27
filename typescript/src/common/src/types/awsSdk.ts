58aed2d7d247ad448f2daf5dcf230ff3e2b3fa64
Anthony Bernabeu <64135631+brnaba-aws@users.noreply.github.com>
2025-03-27 22:51:03 +0100

    prepared ts stack with MAOTS header Added version file generation as part of pre-build script

---
+++ typescript/src/common/src/types/awsSdk.ts
@@ -0,0 +1,24 @@
+/**
+ * Minimal interface for an AWS SDK v3 client.
+ *
+ * @internal
+ */
+interface SdkClient {
+    send: (args: unknown) => Promise<unknown>;
+    config: {
+      serviceId: string;
+    };
+    middlewareStack: {
+      identify: () => string[];
+      addRelativeTo: (middleware: unknown, options: unknown) => void;
+    };
+  }
+
+  /**
+   * Minimal type for the arguments passed to a middleware function
+   *
+   * @internal
+   */
+  type MiddlewareArgsLike = { request: { headers: { [key: string]: string } } };
+
+  export type { SdkClient, MiddlewareArgsLike };
---
+++ typescript/src/common/src/version.ts
@@ -0,0 +1,2 @@
+// this file is auto generated, do not modify
+export const MAOTS_VERSION = '0.1.6';
